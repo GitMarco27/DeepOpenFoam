87db8c5286f9e46cdcef353c82e31939a2063aa6
klajd <klajdi.beqiraj@numerical.it>
2021-12-24 14:26:26 +0100

    Funzione utilizzata per unire pressure e suction side in unica geometria.

---
+++ utils/concatenate_sides.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def concatenate_sides(suction_side, pressure_side):
+    # A label is assigned to identify the side
+    # 0: suction side
+    # 1: pressure side
+    suction_side = np.concatenate((suction_side, np.zeros([suction_side.shape[0], suction_side.shape[1], 1])), axis=2)
+    pressure_side = np.concatenate((pressure_side, np.ones([pressure_side.shape[0], pressure_side.shape[1], 1])),
+                                   axis=2)
+
+    # the two sides are concatenated
+    return np.concatenate((suction_side, pressure_side), axis=1)
