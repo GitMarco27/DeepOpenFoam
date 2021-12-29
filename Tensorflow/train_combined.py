e7f4ce2438680925cf4e42c19235973c3ecd73ec
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2021-12-29 10:00:06 +0100

    Multiprocessing Implementation (WARNING: possible needing to revert this)

--- Tensorflow/train_combined.py
+++ Tensorflow/train_combined.py
@@ -1,17 +1,6 @@
 import tensorflow as tf
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info(tf.__version__)
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-try:
-    for gpu in tf.config.experimental.list_physical_devices("GPU"): tf.config.experimental.set_virtual_device_configuration(
-            gpu,
-            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=12292)])
-except Exception as e:
-    logging.info(e)
-
-
+from multiprocessing import Pool, Manager, Value
 import argparse
 import json
 import os
@@ -26,6 +15,17 @@ from collections import OrderedDict, namedtuple
 import tensorflow as tf
 from utils.concatenate_sides import concatenate_sides
 from utils.PointNetAE import create_pointnet_ae, OrthogonalRegularizer, Sampling
+
+logging.basicConfig(level=logging.INFO)
+logging.info(tf.__version__)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+try:
+    for gpu in tf.config.experimental.list_physical_devices("GPU"): tf.config.experimental.set_virtual_device_configuration(
+            gpu,
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=40000)])
+except Exception as e:
+    logging.info(e)
 
 
 def r_squared(y, y_pred):
@@ -76,10 +76,17 @@ def load_data(path: str = 'dataset'):
 
 
 class RunBuilder:
-    @staticmethod
-    def get_runs(params):
-        Run = namedtuple('Run', params.keys())
+    def __init__(self):
+        pass
 
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, d):
+        self.__dict__ = d
+
+    @staticmethod
+    def get_runs(params, Run):
         runs = []
         for v in product(*params.values()):
             runs.append(Run(*v))
@@ -105,70 +112,23 @@ def handle_results_path():
             os.mkdir(args.log_path)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Training, validation and testing of AE and 3D regressor')
+def single_run(run):
 
-    # Required positional argument
-    parser.add_argument('--data_path', type=str,
-                        default='dataset',
-                        help='dataset path')
+        run, run_count, run_data, args, train_data,\
+            train_labels, val_data, val_labels = run
 
-    parser.add_argument('--results_path', type=str,
-                        default='results',
-                        help='results path')
-    parser.add_argument('--log_path', type=str,
-                        default='logs',
-                        help='log path')
-    parser.add_argument('--clear', type=bool,
-                        default=True,  # @TODO: change
-                        help='delete results path if exists')
-
-    args = parser.parse_args()
-
-    normed_geometries, normed_global_variables, scaler_globals, min_y, max_y = load_data(args.data_path)
-
-    train_data, test_data, train_labels, test_labels = train_test_split(
-        normed_geometries, normed_global_variables, test_size=0.1, shuffle=True)
-
-    train_data, val_data, train_labels, val_labels = train_test_split(
-        train_data, train_labels, test_size=0.1, shuffle=True)
-
-    params = OrderedDict(
-        lr=[.01, .001, .0001],
-        batch_size=[64, 256],
-        epochs=[2500, ],
-        optimizer=['Adam'],
-        type_decoder=['dense', 'cnn'],
-        architectural_parameters=[[False, 0], [True, 1], [True, 5], [True, 10], [True, 15], [True, 20]],
-        # [is_variational, beta],
-        encoding_size=[5, 10, 20, 50, 100],
-        ort_reg_bools=[[False, False], [True, False], [True, True]],  # [feature_transform, orto_reg]
-        reg_drop_out_value=[0., 0.1, 0.3, 0.5]
-    )
-
-    handle_results_path()
-
-    run_count = 0
-    run_data = []
-
-    print('train data', train_data.shape)
-    print('validation data', val_data.shape)
-    print('test data', test_data.shape)
-
-    for run in tqdm.tqdm(RunBuilder.get_runs(params)):
-
-        run_count += 1
+        run_count.value += 1
         print('\n--- New run detected ---')
 
         for key in run._asdict():
             print(f'{key}: {run._asdict()[key]}')
 
-        run_path = os.path.join(args.results_path, str(run_count))
+        run_path = os.path.join(args.results_path, str(run_count.value))
 
         print(f'\n Creating results path: {run_path}')
         os.mkdir(run_path)
 
-        log_dir = os.path.join(args.log_path, str(run_count))
+        log_dir = os.path.join(args.log_path, str(run_count.value))
         os.mkdir(log_dir)
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
         checkpoints_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -216,7 +176,7 @@ if __name__ == '__main__':
         val_scores = model.evaluate(val_data, [val_data, val_labels])
 
         results = OrderedDict()
-        results["run"] = run_count
+        results["run"] = run_count.value
         results["train_loss"], results["train_loss_ae"], results["train_loss_reg"] = train_scores[0], train_scores[1], \
                                                                                      train_scores[2]
         results["val_loss"], results["val_loss_ae"], results["val_loss_reg"] = val_scores[0], val_scores[1], \
@@ -225,15 +185,76 @@ if __name__ == '__main__':
         results["run_path"] = run_path
 
         for k, v in run._asdict().items(): results[k] = v
-        run_data.append(results)
-        df = pd.DataFrame.from_dict(run_data, orient='columns')
+        run_data.value.append(results)
+        df = pd.DataFrame.from_dict(run_data.value, orient='columns')
         df.to_excel(os.path.join(args.results_path, 'results.xlsx'))
 
         with open(os.path.join(run_path, 'log.json'), 'w', encoding='utf-8') as f:
             json.dump([results], f, ensure_ascii=False, indent=4)
 
-    print('\n--- Final Df ---\n')
-    print(df)
+
+params = OrderedDict(
+        lr=[.01, .001, .0001],
+        batch_size=[64, 256],
+        epochs=[2500, ],
+        optimizer=['Adam'],
+        type_decoder=['dense', 'cnn'],
+        architectural_parameters=[[False, 0], [True, 1], [True, 5], [True, 10], [True, 15], [True, 20]],
+        # [is_variational, beta],
+        encoding_size=[5, 10, 20, 50, 100],
+        ort_reg_bools=[[False, False], [True, False], [True, True]],  # [feature_transform, orto_reg]
+        reg_drop_out_value=[0., 0.1, 0.3, 0.5]
+    )
+Run = namedtuple('Run', params.keys())
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Training, validation and testing of AE and 3D regressor')
+
+    # Required positional argument
+    parser.add_argument('--data_path', type=str,
+                        default='dataset',
+                        help='dataset path')
+
+    parser.add_argument('--results_path', type=str,
+                        default='results',
+                        help='results path')
+    parser.add_argument('--log_path', type=str,
+                        default='logs',
+                        help='log path')
+    parser.add_argument('--clear', type=bool,
+                        default=True,  # @TODO: change
+                        help='delete results path if exists')
+
+    args = parser.parse_args()
+
+    normed_geometries, normed_global_variables, scaler_globals, min_y, max_y = load_data(args.data_path)
+
+    train_data, test_data, train_labels, test_labels = train_test_split(
+        normed_geometries, normed_global_variables, test_size=0.1, shuffle=True)
+
+    train_data, val_data, train_labels, val_labels = train_test_split(
+        train_data, train_labels, test_size=0.1, shuffle=True)
+
+    handle_results_path()
+
+    manager = Manager()
+    run_count = manager.Value('run_count', 0)
+    run_data = manager.Value('run_data', [])
+
+    print('train data', train_data.shape)
+    print('validation data', val_data.shape)
+    print('test data', test_data.shape)
+
+    pool = Pool(processes=4)
+
+    pool.map(single_run, [(n, run_count, run_data, args,
+                           train_data, train_labels,
+                           val_data, val_labels) for n in tqdm.tqdm(RunBuilder.get_runs(params, Run))])
+
+    # for run in tqdm.tqdm(RunBuilder.get_runs(params)):
+
+    # print('\n--- Final Df ---\n')
+    # print(df)
 
     # df.sort_values('val_r2', axis=1, inplace=True)
     # best_model = tf.keras.models.load(os.path.join(df.run_path[0], 'model'))
