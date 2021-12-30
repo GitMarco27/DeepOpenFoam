84b45ac339a15210aaccbbe8a1d00f3e94b6b9cd
klajd <klajdi.beqiraj@numerical.it>
2021-12-30 14:42:46 +0100

    minori

---
+++ Tensorflow/utils/load_ae_models.py
@@ -0,0 +1,30 @@
+from GitMarco.tf.metrics import r_squared
+
+from .PointNetAE import create_pointnet_ae, OrthogonalRegularizer, Sampling
+from .custom_objects import chamfer_distance, r_squared
+import tensorflow as tf
+from numpy import save , load
+import os
+import torch
+
+# funzione utilizzata per il caricamento dei modelli utilizzati nell'env
+def load_ae_models(rl_config):
+    ae_model = tf.keras.models.load_model(rl_config['AE']['model_path'],
+                                custom_objects={'r_squared': r_squared,
+                                                     'chamfer_distance': chamfer_distance,
+                                                     'OrthogonalRegularizer': OrthogonalRegularizer,
+                                                     'Sampling': Sampling})
+
+    # divido l'autoencoder in encoder e decoder
+    encoder = ae_model.layers[1]
+    decoder = ae_model.layers[2]
+    reg_model = ae_model.layers[3]
+
+    models= {
+        'ae': ae_model,
+        'reg': reg_model,
+        'encoder': encoder,
+        'decoder': decoder
+    }
+
+    return models
