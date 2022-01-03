89451cbbf4dbf1420b33414536c724ec6819e139
Klajdi Beqiraj <klajdi.beqiraj@numerical.it>
2022-01-03 16:06:54 +0100

    Editing autoencoder functions used in environment

--- Tensorflow/RL_Tools/Environments/env_models_functions.py
+++ Tensorflow/RL_Tools/Environments/env_models_functions.py
@@ -1,21 +1,29 @@
 import numpy as np
 
+def decode(models, latent_param):
+    decoder = models['decoder']
+    scaler_geom = models['scaler_geom']
+    denorm_geom = models['denorm_geom']
 
-# Funzione decode e cal_eff sono usate dall'esterno per la fase di test per generare le geometrie e valutarle
-def decode(decoder, latent_param):
     latent_param = np.expand_dims(latent_param, axis=0).reshape(1, -1)
-    return decoder.predict(latent_param)
+
+    X_norm = decoder.predict(latent_param)
+
+    X = denorm_geom(X_norm,scaler_geom['min_y'], scaler_geom['max_y'])
+    return X
 
 
 def pred_global_variables(latent_param, models):
-    # Convenzione utilizzata, l'utlima variabile della lista deve essere quella
-    # che si vuole ottimizzare
     model_reg = models['reg']
+    scaler_globals =  models['scaler_globals']
 
     # Calcolo delle variabili globali
     latent_param = np.expand_dims(latent_param, axis=0).reshape(1, -1)
-    global_variables = model_reg.predict(latent_param).reshape(-1).tolist()
+    global_variables = model_reg.predict(latent_param)#.reshape(-1).tolist()
+
+    global_variables = scaler_globals.inverse_transform(global_variables).reshape(-1).tolist()
 
     if global_variables[1] == 0.:
         global_variables[1] = 0.00001
+
     return global_variables
